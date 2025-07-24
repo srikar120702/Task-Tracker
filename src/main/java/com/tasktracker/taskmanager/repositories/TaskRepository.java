9942840639c8cdece5c8922a068413019376addd
srikar120702 <70248202+srikar120702@users.noreply.github.com>
2025-07-24 07:43:22 +0530

    committing changes after moving to mongodb from H2

--- src/main/java/com/tasktracker/taskmanager/repositories/TaskRepository.java
+++ src/main/java/com/tasktracker/taskmanager/repositories/TaskRepository.java
@@ -1,9 +1,7 @@
 package com.tasktracker.taskmanager.repositories;
 
 import com.tasktracker.taskmanager.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-@Repository
-public interface TaskRepository extends JpaRepository<Task, String> {
+public interface TaskRepository extends MongoRepository<Task, String> {
 }
